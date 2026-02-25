970b024bc872f02be3c81403a3b5508465228064
Zhumar <avk1512004@gmail.com>
2026-02-25 23:06:44 +0700

    pep8

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,7 +2,10 @@ from posts.models import Group, Post
 from rest_framework import filters, mixins, viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
+)
 
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (
